b4b4c91db3bfdcd73afd299c32aaf907bdaeb401
urielsb <uriel_sbarc@hotmail.com>
2021-11-03 17:38:28 -0600

    Ajustes a la version usando fetch type lazy

--- src/main/java/com/uriel/demoweb/controller/DemoController.java
+++ src/main/java/com/uriel/demoweb/controller/DemoController.java
@@ -2,29 +2,31 @@ package com.uriel.demoweb.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uriel.demoweb.entities.MainEntity;
-import com.uriel.demoweb.repositories.ComplementEntityRepository;
 import com.uriel.demoweb.repositories.MainEntityRepository;
 
 @RestController(value = "/")
 public class DemoController {
 
-	private MainEntityRepository mainEntityRepository;
-	private ComplementEntityRepository complementEntityRepository;
+	private static final Logger log = LoggerFactory.getLogger(DemoController.class);
 	
-	public DemoController(MainEntityRepository mainEntityRepository,
-			ComplementEntityRepository complementEntityRepository) {
-		super();
+	private MainEntityRepository mainEntityRepository;
+	
+	public DemoController(MainEntityRepository mainEntityRepository) {
 		this.mainEntityRepository = mainEntityRepository;
-		this.complementEntityRepository = complementEntityRepository;
 	}
 	
 	@GetMapping("all")
 	public List<MainEntity> findAllMainEntity() {
-		return mainEntityRepository.findAll();
+		log.trace("findAllMainEntity -->");
+		List<MainEntity> result = mainEntityRepository.findAll();
+		log.trace("--> findAllMainEntity");
+		return result;
 	}
 	
 }
